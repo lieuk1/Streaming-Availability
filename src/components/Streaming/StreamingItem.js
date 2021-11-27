44bd89f6c53c96854a95f468a572e529be77e00b
Kristella Lieu <lieuk1@tcnj.edu>
2021-11-26 23:14:50 -0500

    Add separate json files for sub, buy, rent sources

--- src/components/Streaming/StreamingItem.js
+++ src/components/Streaming/StreamingItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Sources from './sources.json';
+import SubSources from './sub-sources.json';
+import BuySources from './buy-sources.json';
+import RentSources from './rent-sources.json';
 
 const StreamingItem = ({ source }) => {
     const sourceID = source.source_id;
@@ -8,7 +10,7 @@ const StreamingItem = ({ source }) => {
     let sourceIcon = <img alt="Source" src="" style={{ width: '50px' }} />;
 
     // Find icon for source in json file using id
-    Sources.map((jsonSource) => {
+    SubSources.map((jsonSource) => {
         if (jsonSource.id === sourceID) {
             sourceIcon = <img alt="Source" src={jsonSource.logo_100px} style={{ border: '1px solid lightgrey', width: '50px' }} />;
         }
