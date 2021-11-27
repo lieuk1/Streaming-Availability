526cde573efadb8f66331fc7b50e044362df2a18
Kristella Lieu <lieuk1@tcnj.edu>
2021-11-26 23:32:49 -0500

    Determine json file to search for source in

--- src/components/Streaming/StreamingItem.js
+++ src/components/Streaming/StreamingItem.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import SubSources from './sub-sources.json';
 import BuySources from './buy-sources.json';
-import RentSources from './rent-sources.json';
+// import RentSources from './rent-sources.json';
 
-const StreamingItem = ({ source }) => {
+const StreamingItem = ({ source, sourceType }) => {
     const sourceID = source.source_id;
 
     // Default icon for source
     let sourceIcon = <img alt="Source" src="" style={{ width: '50px' }} />;
 
+    // Choose which json source list to search in
+    let jsonSources = [];
+    if (sourceType === "sub") {
+        jsonSources = SubSources;
+    } else if (sourceType === "buy") {
+        jsonSources = BuySources;
+    } else {
+        // jsonSources = RentSources;
+    }
+
     // Find icon for source in json file using id
-    SubSources.map((jsonSource) => {
+    jsonSources.map((jsonSource) => {
         if (jsonSource.id === sourceID) {
             sourceIcon = <img alt="Source" src={jsonSource.logo_100px} style={{ border: '1px solid lightgrey', width: '50px' }} />;
         }
