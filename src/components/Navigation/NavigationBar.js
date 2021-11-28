7e3d709e204521474e053d7afe0216449cfbdb42
Kristella Lieu <lieuk1@tcnj.edu>
2021-11-27 21:09:31 -0500

    Add search bar to navbar component

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -1,18 +1,25 @@
+import SearchBar from '../Search/SearchBar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
-const NavigationBar = () => {
+const NavigationBar = ({ onSubmit }) => {
     return (
         <Navbar expand="lg">
             <Container>
                 <Navbar.Brand href="/">STREAMING AVAILABILITY</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
+                <SearchBar onSubmit={onSubmit} />
                 <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="#">Link</Nav.Link>
+                    <Nav.Link href="#">Link</Nav.Link>
+                    <Nav.Link href="#">Link</Nav.Link>
+                    <Nav.Link href="#">Link</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
+                <Button variant="outline-secondary" href="#">Login</Button>
             </Container>
         </Navbar>
     );
