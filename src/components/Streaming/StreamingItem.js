52a84514b78f0e6e25924a7b406a5226f107edd1
Kristella Lieu <lieuk1@tcnj.edu>
2021-11-27 22:02:15 -0500

    Refactor to search in both json files

--- src/components/Streaming/StreamingItem.js
+++ src/components/Streaming/StreamingItem.js
@@ -2,22 +2,14 @@ import React from 'react';
 import SubSources from './sub-sources.json';
 import BuySources from './buy-sources.json';
 
-const StreamingItem = ({ source, sourceType }) => {
+const StreamingItem = ({ source }) => {
     const sourceID = source.source_id;
 
     // Default icon for source
     let sourceIcon = <img alt="Source" src="" style={{ width: '50px' }} />;
 
-    // Choose which json source list to search in
-    let jsonSources = [];
-    if (sourceType === "sub") {
-        jsonSources = SubSources;
-    } else if (sourceType === "buy") {
-        jsonSources = BuySources;
-    }
-
     // Find icon for source in json file using id
-    jsonSources.map((jsonSource) => {
+    [...SubSources, ...BuySources].map((jsonSource) => {
         if (jsonSource.id === sourceID) {
             sourceIcon = <img alt="Source" src={jsonSource.logo_100px} style={{ border: '1px solid lightgrey', width: '50px' }} />;
         }
