f6f32204cb9f942deeebf257680ccbacac108e15
Kristella Lieu <lieuk1@tcnj.edu>
2021-11-29 10:21:49 -0500

    Refactor TitleDetail to use Modal component

--- src/components/Title/TitleDetail.js
+++ src/components/Title/TitleDetail.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import StreamingAvailability from '../Streaming/StreamingAvailability';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
-const TitleDetail = ({ title, subSources, buySources }) => {
+const TitleDetail = ({ show, onHide, title, subSources, buySources }) => {
     if (!title) {
         return <div></div>
     }
 
     return (
-        <div className="ui segment" style={{ width: '100%', marginBottom: '14px', height: '300px' }}>
-            <div className="content">
-                <div className="ui items">
-                    <div className="item">
-                        {/* <div className="image">
-                            <img className="ui large image" alt="Poster Image" src="https://semantic-ui.com/images/wireframe/image.png" />
-                            <img className="ui large image" alt="Poster Image" src="https:cdn.watchmode.com/provider_logos/netflix_100px.png" />
-                        </div> */}
-                        <div className="content">
-                            <div className="header">{title.title} ({title.year})</div>
-                            {title.tmdb_type === 'tv' && <div className="meta">TV Series</div>}
-                            {title.tmdb_type === 'movie' && <div className="meta">Movie</div>}
-                            <div className="description">{title.plot_overview}</div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="content">
-                            <StreamingAvailability subSources={subSources} buySources={buySources} />
-                        </div>
+        <Modal
+            show={show}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+                Modal heading
+            </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="header">{title.title} ({title.year})</div>
+                {title.tmdb_type === 'tv' && <div className="meta">TV Series</div>}
+                {title.tmdb_type === 'movie' && <div className="meta">Movie</div>}
+                <div className="description">{title.plot_overview}</div>
+                <div className="item">
+                    <div className="content">
+                        <StreamingAvailability subSources={subSources} buySources={buySources} />
                     </div>
                 </div>
-            </div>
-        </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
